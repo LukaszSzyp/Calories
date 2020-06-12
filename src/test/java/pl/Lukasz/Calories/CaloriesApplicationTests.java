2c132c8a0446878a3023799f990d0f5c8dccecc8
LukaszSzyp <54202919+LukaszSzyp@users.noreply.github.com>
2020-06-12 20:26:28 +0200

    Add file to open project

---
+++ src/test/java/pl/Lukasz/Calories/CaloriesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lukasz.calories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaloriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
