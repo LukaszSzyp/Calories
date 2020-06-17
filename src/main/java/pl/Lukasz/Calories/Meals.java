b6e8150a5c74431b63c2dc0cf68b1059de5f2d91
LukaszSzyp <54202919+LukaszSzyp@users.noreply.github.com>
2020-06-17 09:46:35 +0200

    RestFull

--- src/main/java/pl/Lukasz/Calories/Meals.java
+++ src/main/java/pl/Lukasz/Calories/Meals.java
@@ -9,15 +9,25 @@ import javax.persistence.Id;
 public class Meals {
 
     private Integer id;
+    private String date;
+    private String time;
     private String text;
+    private Integer calories;
+    private Integer user_id;
+
 
     public Meals(){
     }
 
-    public Meals(Integer id, String text) {
+    public Meals(Integer id, String date, String time, String text, Integer calories, Integer user_id) {
         this.id = id;
+        this.date = date;
+        this.time = time;
         this.text = text;
+        this.calories = calories;
+        this.user_id = user_id;
     }
+
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -37,4 +47,35 @@ public class Meals {
         this.id = id;
     }
 
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public Integer getCalories() {
+        return calories;
+    }
+
+    public void setCalories(Integer calories) {
+        this.calories = calories;
+    }
+
+    public Integer getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
+    }
 }
--- src/main/java/pl/Lukasz/Calories/MealsController.java
+++ src/main/java/pl/Lukasz/Calories/MealsController.java
@@ -1,9 +1,12 @@
 package pl.Lukasz.Calories;
 
 import org.springframework.beans.factory.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 public class MealsController {
@@ -15,4 +18,35 @@ public class MealsController {
     public List<Meals> list() {
         return service.listAll();
     }
+
+    @GetMapping("/meals/{id}")
+    public ResponseEntity<Meals> get(@PathVariable Integer id) {
+        try {
+            Meals meals = service.get(id);
+            return new ResponseEntity<Meals>(meals, HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<Meals>(HttpStatus.NOT_FOUND);
+        }
+    }
+    @PostMapping("/meals/{id}")
+    public ResponseEntity<?> update(@RequestBody Meals meals, @PathVariable Integer id) {
+        try {
+            Meals existMeals = service.get(id);
+            service.save(meals);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/meals")
+    public void add(@RequestBody Meals meals) {
+        service.save(meals);
+    }
+
+    @DeleteMapping("/meals/{id}")
+    public void delete(@PathVariable Integer id) {
+        service.delete(id);
+    }
+
 }
